2c928cc59ebc1c1cfb0805f74672d69e2f14c279
Bruce <brucepark0@gmail.com>
2017-09-11 23:02:29 -0700

    Add tests for atomizing integer and empty string
    Check that having a struct as a key throws an error

--- lib/nested_atomizer.ex
+++ lib/nested_atomizer.ex
@@ -3,6 +3,31 @@ defmodule NestedAtomizer do
   Documentation for NestedAtomizer.
   """
 
-  def atomize(map) do
+  def atomize(map = %{}) do
+    map
+    |> Enum.map(fn{k, v} -> {atomize_key(k), atomize_value(v)} end)
+    |> Enum.into(%{})
   end
+
+  def atomize(pseudo_map), do: pseudo_map
+
+  defp atomize_value(value) when is_map(value) do
+    atomize(value)
+  end
+
+  defp atomize_value(value), do: value
+
+  defp atomize_key(key) when is_map(key) do
+    atomize(key)
+  end
+
+  defp atomize_key(key) when is_binary(key) do
+    String.to_atom(key)
+  end
+
+  defp atomize_key(key) when is_integer(key) do
+    key |> to_string |> atomize_key
+  end
+
+  defp atomize_key(key), do: key
 end
--- test/nested_atomizer_test.exs
+++ test/nested_atomizer_test.exs
@@ -4,15 +4,26 @@ defmodule NestedAtomizerTest do
 
   test "can atomize nested string keys" do
     stringified_map = %{"a" => 2, "b" => 3, %{"c" => 4, "d" => 5} => 6}
-    assert NestedAtomizer.atomize(stringified_map) == %{%{c: 4, d: 5} => 6, a: 2, b: 3}
+    assert NestedAtomizer.atomize(stringified_map) == %{%{:c => 4, :d => 5} => 6, :a => 2, :b => 3}
   end
 
-  test "can atomize a mix of nested string keys and structs, as needed" do
-    naive_datetime = ~N[2000-01-01 23:00:07]
-    stringified_map = %{%{"e" => 4, f: 5} => 6, "a" => 2,
-                        %{"c" => 3, "d" => 4} => 7, naive_datetime => 6}
-    assert NestedAtomizer.atomize(stringified_map) == %{%{c: 3, d: 4} => 7,
-                                                        %{e: 4, f: 5} => 6, :a => 2,
-                                                        naive_datetime => 6}
+  test "throws an error when atomizing a map with a struct key" do
+    date = %Date{year: 2017, month: 3, day: 17}
+    stringified_map = %{%{"e" => 4, :f => 5} => 6, "a" => 2,
+                        %{"c" => 3, "d" => 4, date => 1} => 7}
+    assert_raise Protocol.UndefinedError, ~r/protocol Enumerable not implemented for/,
+      fn -> NestedAtomizer.atomize(stringified_map)
+    end
+  end
+
+  test "can atomize the empty string, as needed" do
+    stringified_map = %{%{"e" => 4, :f => "5"} => 6, "" => [1, 2]}
+    assert NestedAtomizer.atomize(stringified_map) == %{%{:e => 4, :f => "5"} => 6, :"" => [1, 2]}
+  end
+
+  test "can atomize integers, as needed" do
+    stringified_map = %{%{"e" => 4, 0 => 5} => 6, "a" => 2, 3 => 4}
+    assert NestedAtomizer.atomize(stringified_map) == %{%{:e => 4, :"0" => 5} => 6,
+                                                          :a => 2, :"3" => 4}
   end
 end
