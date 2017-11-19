2d9ca2ca5de1bbe3c63bfdd9e4d3a0052079b8d5
Bruce <brucepark0@gmail.com>
2017-11-19 15:14:26 -0800

    Step 3: Allow floating point keys to be stringified
    Allow floating point keys to be stringified
    Tests passing
    Also, adjust to an 80 character column limit
    And fallback to Any when trying to "stringify" a string

--- lib/impl/stringifier/string.ex
+++
@@ -1,3 +0,0 @@
-defimpl Digger.Stringifier.Protocol, for: BitString do
-  def stringify(string, _stringify), do: string
-end
--- test/stringifier_test.exs
+++ test/stringifier_test.exs
@@ -4,27 +4,28 @@ defmodule Digger.StringifierTest do
 
   test "can stringify nested string keys" do
     atomized_map = %{:a => 2, :b => 3, %{:c => 4, :d => 5} => 6}
-    assert Digger.Stringifier.stringify(atomized_map) == %{%{"c" => 4, "d" => 5} => 6,
-      "a" => 2, "b" => 3}
+    assert Digger.Stringifier.stringify(atomized_map) ==
+      %{%{"c" => 4, "d" => 5} => 6, "a" => 2, "b" => 3}
   end
 
   test "does not stringify char lists, lists, or structs like date" do
     date = %Date{year: 2017, month: 3, day: 17}
     atomized_map = %{%{:e => 4, :f => 5} => 6, [1, 2] => 2,
                         %{:c => 3, 'ab' => 4, date => 1} => 7}
-    assert Digger.Stringifier.stringify(atomized_map) == %{%{"e" => 4, "f" => 5} => 6, [1, 2] => 2,
-                                                         %{"c" => 3, 'ab' => 4, date => 1} => 7}
+    assert Digger.Stringifier.stringify(atomized_map) ==
+    %{%{"e" => 4, "f" => 5} => 6, [1, 2] => 2,
+      %{"c" => 3, 'ab' => 4, date => 1} => 7}
   end
 
   test "can stringify the atomized empty string, as needed" do
     atomized_map = %{%{:e => 4, :f => %{:g => "5"}} => 6, :"" => [1, 2]}
-    assert Digger.Stringifier.stringify(atomized_map) == %{%{"e" => 4, "f" => %{"g" => "5"}} => 6,
-                                                              "" => [1, 2]}
+    assert Digger.Stringifier.stringify(atomized_map) ==
+      %{%{"e" => 4, "f" => %{"g" => "5"}} => 6, "" => [1, 2]}
   end
 
-  test "can stringify integers, as needed" do
-    atomized_map = %{%{:e => 4, 0 => 5} => 6, :a => 2, 3 => 4}
-    assert Digger.Stringifier.stringify(atomized_map) == %{%{"e" => 4, "0" => 5} => 6,
-                                                          "a" => 2, "3" => 4}
+  test "can stringify integers and floats, as needed" do
+    atomized_map = %{%{:e => 4, 0 => 5, 1.02 => 2.0} => 6, :a => 2, 3 => 4}
+    assert Digger.Stringifier.stringify(atomized_map) ==
+      %{%{"e" => 4, "0" => 5, "1.02" => 2.0} => 6, "a" => 2, "3" => 4}
   end
 end
