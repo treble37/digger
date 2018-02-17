70aa75bd1048fd6f0b439748b3f18932f2c4f850
Bruce <brucepark0@gmail.com>
2018-02-17 14:15:33 -0800

    Change API of LowerCaser and Dasher API
    Reduce amount of code needed

--- lib/impl/dasher/delegator.ex
+++
@@ -1,5 +0,0 @@
-defmodule Digger.Dasher.Delegator do
-  @moduledoc false
-
-  defdelegate dasherize(data_type), to: Digger.Dasher.Protocol
-end
--- lib/impl/lower_caser/delegator.ex
+++
@@ -1,5 +0,0 @@
-defmodule Digger.LowerCaser.Delegator do
-  @moduledoc false
-
-  defdelegate lowercase_first(data_type), to: Digger.LowerCaser.Protocol
-end
