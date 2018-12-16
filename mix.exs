e3578ace9fe831052624e95bc39320adc800e5b3
Bruce <brucepark0@gmail.com>
2018-12-16 07:44:00 -0800

    Remove support for Elixir 1.3.2
    Do this because credo 1.0.0 has moved to >= Elixir 1.4.0
    For Elixir 1.3.2, running `mix credo` in travis ci yields the following
    error:
    
    ```
    Generated credo app
    Checking 45 source files ...
    ** (UndefinedFunctionError) function Enum.split_with/2 is undefined or private. Did you mean one of:
    * split/2
    * split_while/2
    ```

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Digger.Mixfile do
     [
       app: :digger,
       version: "3.0.0",
-      elixir: ">= 1.3.2",
+      elixir: ">= 1.4.0",
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
       description: description(),
