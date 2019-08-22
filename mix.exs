11fb0ad54634534e646bd0219995db9880ec7818
Bruce <brucepark0@gmail.com>
2019-08-21 22:21:47 -0700

    Update travis tests with elixir 1.9.1

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule Digger.Mixfile do
       {:ex_doc, ">= 0.19.0", only: :dev},
       {:excoveralls, "~> 0.11.2", only: :test},
       {:inch_ex, "~> 2.0.0", only: :docs},
-      {:credo, "~> 1.1.3", only: [:dev, :test]}
+      {:credo, "~> 1.1", only: [:dev, :test]}
     ]
   end
 end
