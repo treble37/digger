43e5368ca4d0f5e8b2b798ff7707c36bf4ba9293
Bruce Park <treble37@users.noreply.github.com>
2026-07-05 16:30:48 -0700

    Bump ex_doc to 0.40.3

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule Digger.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, ">= 0.30.6", only: :dev},
+      {:ex_doc, ">= 0.40.3", only: :dev},
       {:excoveralls, "~> 0.18.5", only: :test},
       {:credo, "~> 1.7.0", only: [:dev, :test]}
     ]
